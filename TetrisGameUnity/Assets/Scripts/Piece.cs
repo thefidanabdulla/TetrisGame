61062a452a7426c9e33e745990d11336d6755b58
Galaxomas <thefidanabdulla@gmail.com>
2022-12-06 13:19:54 +0400

    piece rotation updated

--- TetrisGameUnity/Assets/Scripts/Piece.cs
+++ TetrisGameUnity/Assets/Scripts/Piece.cs
@@ -81,6 +81,25 @@ public class Piece : MonoBehaviour
 
         for(int i = 0; i < this.cells.Length; i++) { 
             Vector3 cell = this.cells[i];
+
+            int x, y;
+
+            switch (this.data.tetromino)
+            {
+                case Tetromino.I:
+                case Tetromino.O:
+                    cell.x -= 0.5f;
+                    cell.y -= 0.5f;
+                    x = Mathf.CeilToInt((cell.x * Data.RotationMatrix[0] * direction) + (cell.y * Data.RotationMatrix[1] * direction));
+                    y = Mathf.CeilToInt((cell.x * Data.RotationMatrix[2] * direction) + (cell.y * Data.RotationMatrix[3] * direction));
+                    break;
+                default:
+                    x = Mathf.RoundToInt((cell.x * Data.RotationMatrix[0] * direction) + (cell.y * Data.RotationMatrix[1] * direction));
+                    y = Mathf.RoundToInt((cell.x * Data.RotationMatrix[2] * direction) + (cell.y * Data.RotationMatrix[3] * direction));
+                    break;
+            }
+
+            this.cells[i] = new Vector3Int(x, y, 0);    
         }
     }
 
